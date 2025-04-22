e039dbb811f4ca95fc5505be4e6c063adf06b3c5
timbr <tibrowning@gmail.com>
2025-04-22 12:47:45 +0000

    TB fix chapter 4

--- orm.py
+++ orm.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey
-from sqlalchemy.orm import mapper, relationship
+from sqlalchemy import Table, Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import registry, relationship
 
 import model
 
+mapper_registry = registry()
 
-metadata = MetaData()
+metadata = mapper_registry.metadata
 
 order_lines = Table(
     "order_lines",
@@ -35,8 +36,8 @@ allocations = Table(
 
 
 def start_mappers():
-    lines_mapper = mapper(model.OrderLine, order_lines)
-    mapper(
+    lines_mapper = mapper_registry.map_imperatively(model.OrderLine, order_lines)
+    mapper_registry.map_imperatively(
         model.Batch,
         batches,
         properties={
